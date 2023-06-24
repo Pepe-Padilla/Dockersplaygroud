34ad9895b54de47414a25c7d5caa9510ef69a04a
José Padilla <jpadilla.isc@gmail.com>
2023-06-24 12:33:35 +0200

    more pro moves
    basicamente dejarlo bien D:

--- terminallog.sh
+++ terminallog.sh
@@ -5,12 +5,14 @@ docker network create goals-net
 
 #Start the networks:
 ## mongoDB
-docker run --name mongodb --rm -d --network goals-net mongo
+docker run --name mongodb -v mongoData:/data/db --rm -d --network goals-net -e MONGO_INITDB_ROOT_USERNAME=usrmongo -e MONGO_INITDB_ROOT_PASSWORD=secret mongo
 
 ## backend
 cd ./backend
-docker run --name goals-backend --rm -d --network goals-net goals-node
+docker build -t goals-node .
+docker run --name goals-backend --rm -d -p 80:80 --network goals-net goals-node
 
 ## frontend
 cd ../frontend
-docker 
+docker build -t goals-react .
+docker run --name goals-frontend --rm -it -p 3000:3000 goals-react
